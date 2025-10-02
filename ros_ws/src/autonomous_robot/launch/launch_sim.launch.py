e8181a7e0ce1aaaf7fef056ac2b9535f34355115
jonathanrandall <jonnyrandall@live.com.au>
2025-10-02 14:11:36 +1000

    some updates for navigation

--- ros_ws/src/autonomous_robot/launch/launch_sim.launch.py
+++ ros_ws/src/autonomous_robot/launch/launch_sim.launch.py
@@ -45,7 +45,7 @@ def generate_launch_description():
     default_world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
-        'empty.world'
+        'empty.sdf'
         )   
 
     rviz_file = os.path.join(
