2d1a0cccdfa068035b5108ffd1861ab82fbc05f3
jonathanrandall <jonnyrandall@live.com.au>
2025-11-08 22:15:01 +1100

    pi launch files

--- ros_ws/src/autonomous_robot/launch/pi.launch.py
+++ ros_ws/src/autonomous_robot/launch/pi.launch.py
@@ -1,19 +1,15 @@
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 import os
+from ament_index_python.packages import get_package_share_directory
 import yaml
 from tempfile import NamedTemporaryFile
 
 from ament_index_python.packages import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessStart
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-
-from launch_ros.actions import Node
 
 def prepend_namespace_to_yaml(namespace, yaml_path):
     with open(yaml_path, 'r') as f:
@@ -26,70 +22,69 @@ def prepend_namespace_to_yaml(namespace, yaml_path):
 
 
 def generate_launch_description():
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("pan_tilt_hardware"), "urdf", "pan_tilt_standalone_pi.xacro"]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
 
-    package_name='autonomous_robot'
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        namespace="pi",
+        output='screen',
+        parameters=[robot_description]
+    )
 
-    xacro_file = PathJoinSubstitution([
-        FindPackageShare(package_name),
-        "description",
-        "robot.urdf.xacro"
-    ])
 
-    robot_description = {
-        "robot_description": Command([
-            "xacro", " ", xacro_file,
-            " use_ros2_control:=true",
-            " sim_mode:=false"
-        ])
-    }
-
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+    controller_params_file = os.path.join(get_package_share_directory("pan_tilt_hardware"),'config','pan_tilt_controllers.yaml')
 
     namespace = "pi"
     namespaced_yaml = prepend_namespace_to_yaml(namespace, controller_params_file)
 
-    controller_manager = Node(
+    # Controller manager node
+    control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         namespace="pi",
-        parameters=[robot_description,
-                    namespaced_yaml],
-        remappings=[('/pi/robot_description', '/robot_description')],
+        parameters=[robot_description, namespaced_yaml],
+        # remappings=[('/pi/robot_description', '/robot_description')],
         output="screen",
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    # Joint state broadcaster spawner
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/pi/controller_manager"],
+    )
 
-    # Only spawn pan_tilt controller and joint state broadcaster for the Pi
-    pan_tilt_spawner = Node(
+    # Pan-tilt controller spawner
+    pan_tilt_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["pan_tilt_controller", "--controller-manager", "/pi/controller_manager"],
     )
 
-    delayed_pan_tilt_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[pan_tilt_spawner],
+    # Delay pan_tilt_controller start after joint_state_broadcaster
+    delay_pan_tilt_controller_spawner_after_joint_state_broadcaster = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[pan_tilt_controller_spawner],
         )
     )
 
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_broad", "--controller-manager", "/pi/controller_manager"],
-    )
+    nodes = [
+        node_robot_state_publisher,
+        control_node,
+        joint_state_broadcaster_spawner,
+        delay_pan_tilt_controller_spawner_after_joint_state_broadcaster,
+    ]
 
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner],
-        )
-    )
-
-    # Launch them all!
-    return LaunchDescription([
-        delayed_controller_manager,
-        delayed_pan_tilt_spawner,
-        delayed_joint_broad_spawner
-    ])
+    return LaunchDescription(nodes)
